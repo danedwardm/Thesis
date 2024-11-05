d114b7a90dce0a400ad89b4512c1e4c7c4dd12fa
Sean Lowie <sean.bearbone@gmail.com>
2024-11-05 15:55:52 +0800

    may inayos lang

--- website/src/Pages/MainAdmin/Dashboard.jsx
+++ website/src/Pages/MainAdmin/Dashboard.jsx
@@ -68,41 +68,110 @@ const Dashboard = () => {
   }, [googleMapsApiKey, location]); // Re-run whenever location changes
 
   return (
-    <>
-      <div className="relative bg-second h-screen w-screen overflow-hidden flex flex-col">
-        {/* bg square */}
-        <div className="z-10">
-          <div className="absolute h-[30vh] w-[30vh] bg-square rounded-[20px] rotate-45 -top-24 -left-24"></div>
-          <div className="absolute h-[30vh] w-[30vh] bg-square rounded-[20px] rotate-45 top-2/3 left-0"></div>
-          <div className="absolute h-[30vh] w-[30vh] bg-square rounded-[20px] rotate-45 top-0 left-1/3"></div>
-          <div className="absolute h-[30vh] w-[30vh] bg-square rounded-[20px] rotate-45 -top-40 -right-10"></div>
-          <div className="absolute h-[30vh] w-[30vh] bg-square rounded-[20px] rotate-45 top-96 left-2/3"></div>
+    <div className="relative bg-second h-screen w-screen overflow-hidden flex flex-col">
+      {/* Background squares */}
+      <div className="z-10">
+        <div className="absolute h-[30vh] w-[30vh] bg-square rounded-[20px] rotate-45 -top-24 -left-24"></div>
+        <div className="absolute h-[30vh] w-[30vh] bg-square rounded-[20px] rotate-45 top-2/3 left-0"></div>
+        <div className="absolute h-[30vh] w-[30vh] bg-square rounded-[20px] rotate-45 top-0 left-1/3"></div>
+        <div className="absolute h-[30vh] w-[30vh] bg-square rounded-[20px] rotate-45 -top-40 -right-10"></div>
+        <div className="absolute h-[30vh] w-[30vh] bg-square rounded-[20px] rotate-45 top-96 left-2/3"></div>
+      </div>
+
+      {/* Main content */}
+      <div className="relative w-full flex flex-col items-center z-30 overflow-auto flex-grow">
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Stats cards */}
+        <div className="w-full md:mt-[21vh] mt-[30vh] grid md:grid-cols-4 grid-cols-2 md:gap-6 gap-2 px-6">
+          {/* Number of calls */}
+          <div className="col-span-1 m-3 md:px-3 py-5 text-2xl font-bold text-main mb-2 bg-white border border-main rounded-lg flex flex-col md:flex-row md:gap-2 gap-1 justify-between items-center overflow-hidden">
+            <div className="lg:flex justify-center items-center ml-3 hidden">
+              <div className="flex justify-center items-center rounded-full bg-main w-[55px] h-[55px]">
+                <IoCall className="text-second w-[30px] h-[30px]" />
+              </div>
+            </div>
+            <div className="flex justify-center items-center md:ml-3 w-full">
+              <p className="font-bold lg:text-sm text-xs text-center md:text-start uppercase text-main w-full">
+                Daily Calls
+              </p>
+            </div>
+            <div className="flex justify-center items-center gap-2 md:ml-3 w-full">
+              <div className="rounded-full text-main md:text-4xl text-xl">11</div>
+            </div>
+          </div>
+
+          {/* Active Reports */}
+          <div className="col-span-1 m-3 md:px-3 py-5 text-2xl font-bold text-main mb-2 bg-white border border-main rounded-lg flex flex-col md:flex-row md:gap-2 gap-1 justify-between items-center overflow-hidden">
+            <div className="lg:flex justify-center items-center ml-3 hidden">
+              <div className="flex justify-center items-center rounded-full bg-main w-[55px] h-[55px]">
+                <MdReport className="text-second w-[30px] h-[30px]" />
+              </div>
+            </div>
+            <div className="flex justify-center items-center md:ml-3 w-full">
+              <p className="font-bold lg:text-sm text-xs text-center md:text-start uppercase text-main w-full">
+                Active Reports
+              </p>
+            </div>
+            <div className="flex justify-center items-center gap-2 md:ml-3 w-full">
+              <div className="rounded-full text-main md:text-4xl text-xl">11</div>
+            </div>
+          </div>
+
+          {/* Weekly Reports */}
+          <div className="col-span-1 m-3 md:px-3 py-5 text-2xl font-bold text-main mb-2 bg-white border border-main rounded-lg flex flex-col md:flex-row md:gap-2 gap-1 justify-between items-center overflow-hidden">
+            <div className="lg:flex justify-center items-center ml-3 hidden">
+              <div className="flex justify-center items-center rounded-full bg-main w-[55px] h-[55px]">
+                <HiOutlineDocumentReport className="text-second w-[30px] h-[30px]" />
+              </div>
+            </div>
+            <div className="flex justify-center items-center md:ml-3 w-full">
+              <p className="font-bold lg:text-sm text-xs text-center md:text-start uppercase text-main w-full">
+                Weekly Reports
+              </p>
+            </div>
+            <div className="flex justify-center items-center gap-2 md:ml-3 w-full">
+              <div className="rounded-full text-main md:text-4xl text-xl">11</div>
+            </div>
+          </div>
+
+          {/* Number of Users */}
+          <div className="col-span-1 m-3 md:px-3 py-5 text-2xl font-bold text-main mb-2 bg-white border border-main rounded-lg flex flex-col md:flex-row md:gap-2 gap-1 justify-between items-center overflow-hidden">
+            <div className="lg:flex justify-center items-center ml-3 hidden">
+              <div className="flex justify-center items-center rounded-full bg-main w-[55px] h-[55px]">
+                <FaUser className="text-second w-[30px] h-[30px]" />
+              </div>
+            </div>
+            <div className="flex justify-center items-center md:ml-3 w-full">
+              <p className="font-bold lg:text-sm text-xs text-center md:text-start uppercase text-main w-full">
+                Number of Users
+              </p>
+            </div>
+            <div className="flex justify-center items-center gap-2 md:ml-3 w-full">
+              <div className="rounded-full text-main md:text-4xl text-xl">11</div>
+            </div>
+          </div>
         </div>
-        {/* content */}
-        <div className="relative w-full flex flex-col items-center z-30 overflow-auto flex-grow">
-          <Navbar />
-          <div className="w-full md:mt-[21vh] mt-[30vh] grid md:grid-cols-4 grid-cols-2 md:gap-6 gap-2 px-6">
-            {/* Other dashboard items here */}
+
+        {/* Filter section */}
+        <div className="w-full grid grid-cols-2 justify-between items-center md:mt-3 mt-4 px-6">
+          <div className="flex flex-col px-3">
+            <p className="md:text-xl text-sm font-bold text-main">Map of Reports</p>
           </div>
-          {/* filter */}
-          <div className="w-full grid grid-cols-2 justify-between items-center md:mt-3 mt-4 px-6">
-            <div className="flex flex-col px-3">
-              <p className="md:text-xl text-sm font-bold text-main">map of reports</p>
-            </div>
-            {/* filter button */}
-            <div className="flex flex-col justify-center items-end px-3">
-              <button className="flex justify-center items-center rounded-md bg-white border-main border text-xs text-main font-bold md:px-5 md:py-2 px-3 py-1 uppercase hover:bg-textSecond hover:text-accent ease-in-out duration-500">
-                Filter
-              </button>
-            </div>
+          <div className="flex flex-col justify-center items-end px-3">
+            <button className="flex justify-center items-center rounded-md bg-white border-main border text-xs text-main font-bold md:px-5 md:py-2 px-3 py-1 uppercase hover:bg-textSecond hover:text-accent ease-in-out duration-500">
+              Filter
+            </button>
           </div>
-          {/* map */}
-          <div className="w-full flex-grow bg-black md:mt-5 mt-3 border-2 border-t-main">
-            <div ref={mapRef} style={mapContainerStyle}></div>
-          </div>
+        </div>
+
+        {/* Map */}
+        <div className="w-full flex-grow bg-black md:mt-5 mt-3 border-2 border-t-main">
+          <div ref={mapRef} style={mapContainerStyle}></div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
