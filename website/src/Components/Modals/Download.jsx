42269423c53206320eb0bc9f0abc9cdbffd74026
dalandan <danedwardm@gmail.com>
2025-03-19 15:29:45 +0800

    updated

--- website/src/Components/Modals/Download.jsx
+++ website/src/Components/Modals/Download.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import QRcode from "../../assets/QRcode.png";
+import QRcode from "../../assets/frame.png";
 
 const Download = ({ isVisible, onClose }) => {
   if (!isVisible) return null;
@@ -36,7 +36,7 @@ const Download = ({ isVisible, onClose }) => {
               </div>
               <div className="w-full flex flex-row gap-4 items-center justify-end mt-5">
                 <a
-                  href="https://tinyurl.com/msrv4m6s"
+                  href="https://tinyurl.com/446s5u43"
                   className="py-2 px-3 border border-main bg-main text-white rounded-lg text-xs font-bold hover:scale-105 ease-in-out duration-500 truncate"
                 >
                   DOWNLOAD
